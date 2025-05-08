b286dcf5fca18670beac9c29ed039c50e23d8e09
github-ci <you@example.com>
2025-05-08 10:48:47 +0000

    fix+++

--- src/Repository/MediaCheckRepository.php
+++ src/Repository/MediaCheckRepository.php
@@ -4,7 +4,6 @@ namespace WechatMiniProgramSecurityBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use DoctrineEnhanceBundle\Repository\CommonRepositoryAware;
 use WechatMiniProgramSecurityBundle\Entity\MediaCheck;
 
 /**
@@ -15,7 +14,6 @@ use WechatMiniProgramSecurityBundle\Entity\MediaCheck;
  */
 class MediaCheckRepository extends ServiceEntityRepository
 {
-    use CommonRepositoryAware;
 
     public function __construct(ManagerRegistry $registry)
     {
--- src/Repository/RiskLogRepository.php
+++ src/Repository/RiskLogRepository.php
@@ -4,7 +4,6 @@ namespace WechatMiniProgramSecurityBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
-use DoctrineEnhanceBundle\Repository\CommonRepositoryAware;
 use WechatMiniProgramSecurityBundle\Entity\RiskLog;
 
 /**
@@ -15,7 +14,6 @@ use WechatMiniProgramSecurityBundle\Entity\RiskLog;
  */
 class RiskLogRepository extends ServiceEntityRepository
 {
-    use CommonRepositoryAware;
 
     public function __construct(ManagerRegistry $registry)
     {
