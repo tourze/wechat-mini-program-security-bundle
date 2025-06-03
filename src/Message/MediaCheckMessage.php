8d0d5f1065ef18671fab4c86b9479a7fa1910316
tourze <tourze@users.noreply.github.com>
2025-06-03 16:56:14 +0800

    update

--- src/Message/MediaCheckMessage.php
+++ src/Message/MediaCheckMessage.php
@@ -2,7 +2,7 @@
 
 namespace WechatMiniProgramSecurityBundle\Message;
 
-use Tourze\Symfony\Async\Message\AsyncMessageInterface;
+use Tourze\AsyncContracts\AsyncMessageInterface;
 
 class MediaCheckMessage implements AsyncMessageInterface
 {
