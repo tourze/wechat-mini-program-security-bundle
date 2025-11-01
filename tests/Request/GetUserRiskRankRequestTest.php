a156e9044f441f488cdc33d351628277ae7fcc58
github-ci <you@example.com>
2025-11-02 03:30:58 +0800

    update++++++

--- tests/Request/GetUserRiskRankRequestTest.php
+++ tests/Request/GetUserRiskRankRequestTest.php
@@ -2,7 +2,7 @@
 
 namespace WechatMiniProgramSecurityBundle\Tests\Request;
 
-use HttpClientBundle\Tests\Request\RequestTestCase;
+use HttpClientBundle\Test\RequestTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 use WechatMiniProgramSecurityBundle\Request\GetUserRiskRankRequest;
 
